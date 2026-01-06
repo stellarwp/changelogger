895f4bdc7d8a252fa44374e40a05fe1e0b785de7
Eric Defore <eric.defore@gmail.com>
2026-01-06 15:18:02 -0500

    Remove accidentally added files during merge
    These files are not versioned. Yet. Maybe will fix with #142

--- dist/gha/github.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * GitHub Actions entry point
- * This function handles the GitHub Actions workflow by processing the inputs
- * and executing the appropriate command.
- */
-export declare function run(): Promise<void>;
